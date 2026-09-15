90ce87f45c6caf77641c5e1b1fdd4a4703f0ab58
KongZhipeng <chara1997@foxmail.com>
2026-09-15 15:04:38 +0800

    fix: 页脚 GitHub 链接指向真实仓库地址

--- src/data/site.js
+++ src/data/site.js
@@ -4,5 +4,5 @@ export const site = {
   tagline: '记录学习与生活',
   author: 'KongZhipeng',
   since: 2026,
-  github: 'https://github.com/your-username/ViteBlog', // 换成你自己的仓库地址
+  github: 'https://github.com/KongValley/ViteBlog',
 }
